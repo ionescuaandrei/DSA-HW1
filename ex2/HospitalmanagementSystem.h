d718ea54e5a30bd2f86ef95305602d67d02462e9
Andrei Ionescu <129840688+ionescuaandrei@users.noreply.github.com>
2025-04-04 04:16:02 +0300

    ex2 done + tests done + do review @Stefan5419

--- ex2/HospitalmanagementSystem.h
+++ ex2/HospitalmanagementSystem.h
@@ -1,54 +1,96 @@
-//
-// Created by ionescuaandrei on 4/4/25.
-//
-
-#ifndef HOSPITALMANAGEMENTSYSTEM_H
-#define HOSPITALMANAGEMENTSYSTEM_H
-
-#endif //HOSPITALMANAGEMENTSYSTEM_H
-
 #include "Patient.h"
-#include "../ds/list1.h"
+#include "list1.h"
 #include <string>
 using namespace std;
 
-class HospitalManagementSystem{
-    private:
-        LinkedList<Patient> beds;
+class HospitalManagementSystem {
+private:
+    LinkedList<Patient> beds;
 
-    public:
-
-        void addPatient(string& name, string& condition);
-        void dischargePatient(int bedIndex);
-        void transferPatient(int sourceBedIndex, int targetBedIndex);
-        void displaySystem();
-        string getPatientInfo(int bedIndex);
-
+public:
+    void admitPatient(string& name, string& condition);
+    void dischargePatient(int bedIndex);
+    void transferPatient(int sourceBedIndex, int targetBedIndex);
+    void displaySystem();
+    string getPatientInfo(int bedIndex);
 };
-void HospitalManagementSystem::addPatient(string& name, string& condition){
+
+void HospitalManagementSystem::admitPatient(string& name, string& condition) {
     Patient p;
     p.name = name;
     p.condition = condition;
-    beds.addFirst(p);
+    beds.addLast(p);
 }
 
-void HospitalManagementSystem::dischargePatient(int bedIndex){
-    Node<Patient>* paux = beds.pfirst;
-    for(int i = 0; i < bedIndex; i++){
-        if(paux != NULL)
-            paux = paux->next;
-        else
-            return;
+void HospitalManagementSystem::displaySystem() {
+    Node<Patient>* displayBed = beds.pfirst;
+    int wardNumber = 0;
+    int bedIndex = 0;
+    while (displayBed != NULL) {
+        if (bedIndex % 4 == 0) {
+            // if (bedIndex > 0) {
+                cout << endl;
+                cout << "Ward"  << wardNumber << endl;
+                wardNumber++;
+            // }
+        }
+        cout << "Bed " << bedIndex << ": " << displayBed->info.name << " (" << displayBed->info.condition << ") ";
+        displayBed = displayBed->next;
+        bedIndex++;
     }
-    if(paux != NULL)
-        beds.removeLastOccurrence(paux->info);
+    cout << endl;
 }
 
+void HospitalManagementSystem::dischargePatient(int bedIndex) {
+    int size = 0;
+    Node<Patient>* current = beds.pfirst;
+    while (current != NULL) {
+        size++;
+        current = current->next;
+    }
 
-void HospitalManagementSystem::displaySystem(){
-    Node<Patient>* paux = beds.pfirst;
-    while(paux != NULL){
-        cout << "Name: " << paux->info.name << ", Condition: " << paux->info.condition << endl;
-        paux = paux->next;
+    switch (size) {
+        case 0:
+            cout << "nothing to discharge" << endl;
+        return;
+        case 1:
+            cout << "discharging the only patient" << endl;
+        beds.removeAt(0);
+        return;
+        default:
+            if (bedIndex < 0 || bedIndex >= size) {
+                cout << "Invalid bed index" << endl;
+                return;
+            }
+        cout << "discharging patient from bed " << bedIndex << endl;
+        beds.removeAt(bedIndex);
     }
 }
+
+void HospitalManagementSystem::transferPatient(int sourceBedIndex, int targetBedIndex) {
+    if (sourceBedIndex == targetBedIndex) return;
+
+    Node<Patient>* sourceNode = beds.pfirst;
+    for (int i = 0; sourceNode != NULL && i < sourceBedIndex; i++) {
+        sourceNode = sourceNode->next;
+    }
+    if (!sourceNode) return;
+
+    Patient patient = sourceNode->info;
+
+    beds.removeAt(sourceBedIndex);
+
+    if (sourceBedIndex < targetBedIndex) targetBedIndex--;
+
+    beds.insertAt(targetBedIndex+1 , patient);
+}
+
+string HospitalManagementSystem::getPatientInfo(int bedIndex){
+    Node<Patient>* bedInfo = beds.pfirst;
+    for (int i = 0; bedInfo != NULL && i < bedIndex; i++) {
+        bedInfo = bedInfo->next;
+    }
+    if (bedInfo == NULL) return "Invalid bed index";
+    return "Name: " + bedInfo->info.name + ", Condition: " + bedInfo->info.condition;
+}
+
--- ex2/Patient.h
+++ ex2/Patient.h
@@ -1,9 +1,8 @@
-
-
 #include <string>
 using namespace std;
 
 struct Patient{
     string name;
     string condition;
+    int bedIndex;
 };
